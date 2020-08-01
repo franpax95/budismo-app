b1aa2ef4041cdce8f963dca78785358a7cc47577
Palumbo <fcojnavarrowork@gmail.com>
2020-08-01 21:48:29 +0200

    Actualizando el contenido (data.json) y el display de la barra de navegación acorde al contenido

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './styles.css';
+import data from '../../data.json';
 
 const Navbar = ({ onClick }) => {
     return(
         <div className="Navbar">
-            <button id="0" onClick={onClick}>Intro</button>
+            {data.map(x => <button key={x.id} id={x.id} onClick={onClick}>{x.title}</button>)}
+            {/* <button id="0" onClick={onClick}>Intro</button>
             <button id="1" onClick={onClick}>La historia</button>
             <button id="2" onClick={onClick}>Curiosidades</button>
-            <button id="3" onClick={onClick}>Sección 4</button>
+            <button id="3" onClick={onClick}>Sección 4</button> */}
         </div>
     );
 }
