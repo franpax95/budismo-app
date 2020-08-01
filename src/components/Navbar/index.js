4fb662309fe5ee2e7b3c5cfe1b6069b4bc106c51
Palumbo <fcojnavarrowork@gmail.com>
2020-08-01 21:51:33 +0200

    Actualizado favicon

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,10 +6,6 @@ const Navbar = ({ onClick }) => {
     return(
         <div className="Navbar">
             {data.map(x => <button key={x.id} id={x.id} onClick={onClick}>{x.title}</button>)}
-            {/* <button id="0" onClick={onClick}>Intro</button>
-            <button id="1" onClick={onClick}>La historia</button>
-            <button id="2" onClick={onClick}>Curiosidades</button>
-            <button id="3" onClick={onClick}>Sección 4</button> */}
         </div>
     );
 }
